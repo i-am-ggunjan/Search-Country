b4039350498a3bfda0be6902e53d658acbcdbdbb
Gaurav Gunjan <gauravgunjan200301@gmail.com>
2023-03-03 01:12:06 +0530

    state concept use for data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Search2 from "./components/Search2";
+// import Search2 from "./components/Search2";
+import Search3 from "./components/Search3";
 
 const App = () => {
   return (
     <>
-      <Search2></Search2>
+      <Search3></Search3>
     </>
   );
 };
